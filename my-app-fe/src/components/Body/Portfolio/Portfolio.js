a1811e7afa98ce38cdb5b325df677ce0adcaf695
GioLobaton <giolobaton1024@gmail.com>
2023-03-30 19:27:23 +0800

    Portfolio

---
+++ my-app-fe/src/components/Body/Portfolio/Portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../Header/Header';
+import Footer from '../../Footer/Footer';
+
+function Portfolio(props) {
+    return (
+        <>
+        <Header />
+            <h1>This is porfolio - Gio Lobaton</h1> 
+         <Footer/>   
+        </>
+    );
+}
+
+export default Portfolio;
