012041d1db4966d7881b2d1cdc2c216af60fe4b9
ajmdz <contact.ajmendoza@gmail.com>
2023-03-30 19:29:38 +0800

    sample

---
+++ my-app-fe/src/components/Body/Sample/Sample.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../Header/Header';
+import Footer from '../../Footer/Footer';
+
+
+function Sample(props) {
+    return (
+       <>
+       <Header />
+       <h1> Sample AJ </h1>
+       <Footer />
+       </> 
+    );
+}
+
+export default Sample;
