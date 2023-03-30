c11ed1be60da1d7ceee9e6e1450c640e3c427edb
Jerwin Cruz <cruz.jerwin15@gmail.com>
2023-03-30 19:50:19 +0800

    fix bug 2

--- my-app-fe/src/components/Header/Header.js
+++ my-app-fe/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-
+S
 
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -9,10 +9,9 @@ function Header() {
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="about-us">About Us</Link></li>
-                <li><Link to="contact-us">Contact Us</Link></li>
-                <li><Link to ="portfolio">Portfolio</Link></li>
-                <li><Link to ="sample">Sample</Link></li>
-
+                <li>Contact Us</li>
+                <li>Portfolio</li>
+                <li>Sample</li>
             </ul> 
         </>
     );
