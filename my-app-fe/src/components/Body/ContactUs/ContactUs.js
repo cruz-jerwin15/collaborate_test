47cf06fe5dd50353a458da1be2845b4e6eb6507d
Jean Eileen Magtibay <s2020102878@firstasia.edu.ph>
2023-03-30 19:25:46 +0800

    Jean's commit

--- my-app-fe/src/components/Body/ContactUs/ContactUs.js
+++ my-app-fe/src/components/Body/ContactUs/ContactUs.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Footer from '../../Footer/Footer';
 import Header from '../../Header/Header';
 
-function AboutUs() {
+function ContactUs(props) {
     return (
         <>
-            <Header/>
-            <h1>About us</h1>   
-            <Footer/>
+           <Header/>
+            <h1>Contact Us</h1>
+           <Footer/> 
         </>
     );
 }
 
-export default AboutUs;
+export default ContactUs;
